21e4dd37fdff0858f07816ba0b7b872fc48203b2
Nishu2903 <117971452+Nishu2903@users.noreply.github.com>
2023-03-28 23:19:13 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 class BusyTerminal(MRJob):
 
@@ -7,7 +8,7 @@ class BusyTerminal(MRJob):
         fields = line.strip().split(',')
 
         # Extract the Terminal and Passenger Count fields
-        terminal = fields[2]
+        terminal = fields[1]
         passenger_count = int(fields[3])
 
         # Emit a key-value pair with the Terminal as the key and the Passenger Count as the value
@@ -20,24 +21,9 @@ class BusyTerminal(MRJob):
         # Emit a key-value pair with the Terminal as the key and the total count as the value
         yield terminal, total_count
 
-    def mapper_light(self, terminal, count):
-        # Check if the flight has a "light" designation
-        if count < 50:
-            # Emit a key-value pair with the Terminal as the key and a count of 1 as the value
-            yield terminal, 1
-
-    def reducer_light(self, terminal, counts):
-        # Count the number of "light" flights for each Terminal
-        light_count = sum(counts)
-
-        # Emit a key-value pair with the Terminal as the key and the number of "light" flights as the value
-        yield terminal, light_count
-
     def steps(self):
         return [
-            self.mr(mapper=self.mapper, reducer=self.reducer),
-            self.mr(mapper=self.mapper_light, reducer=self.reducer_light)
-        ]
+            MRStep(mapper=self.mapper, reducer=self.reducer)        ]
 
 if __name__ == '__main__':
     BusyTerminal.run()
