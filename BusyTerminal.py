a5588090cc8ceda2703f41a45193ea99d6a63099
Nishu2903 <117971452+Nishu2903@users.noreply.github.com>
2023-03-28 22:43:21 +0530

    Update BusyTerminal.py

--- BusyTerminal.py
+++ BusyTerminal.py
@@ -1,27 +1,42 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
 
 class BusyTerminal(MRJob):
 
     def mapper(self, _, line):
+        # Split the line into fields
         fields = line.strip().split(',')
-        terminal = fields[1]
+
+        # Extract the Terminal and Passenger Count fields
+        terminal = fields[2]
         passenger_count = int(fields[3])
+
+        # Emit a key-value pair with the Terminal as the key and the Passenger Count as the value
         yield terminal, passenger_count
 
     def reducer(self, terminal, counts):
+        # Sum up the counts for each Terminal
         total_count = sum(counts)
+
+        # Emit a key-value pair with the Terminal as the key and the total count as the value
         yield terminal, total_count
 
+    def mapper_light(self, terminal, count):
+        # Check if the flight has a "light" designation
+        if count < 50:
+            # Emit a key-value pair with the Terminal as the key and a count of 1 as the value
+            yield terminal, 1
+
     def reducer_light(self, terminal, counts):
-        light_counts = filter(lambda x: x < 50, counts)
-        light_count = sum(1 for _ in light_counts)
+        # Count the number of "light" flights for each Terminal
+        light_count = sum(counts)
+
+        # Emit a key-value pair with the Terminal as the key and the number of "light" flights as the value
         yield terminal, light_count
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper, reducer=self.reducer),
-            MRStep(reducer=self.reducer_light)
+            self.mr(mapper=self.mapper, reducer=self.reducer),
+            self.mr(mapper=self.mapper_light, reducer=self.reducer_light)
         ]
 
 if __name__ == '__main__':
